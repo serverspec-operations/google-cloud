75700cb93128f4f7068eddb8a4d2767e34e8946b
Gosuke Miyashita <gosukenator@gmail.com>
2024-09-03 22:22:41 +0900

    Update network settings

--- projects/shared-vpc-host/static_outbound_ip.tf
+++ projects/shared-vpc-host/static_outbound_ip.tf
@@ -4,32 +4,43 @@ resource "google_compute_network" "static_outbound_ip" {
 }
 
 ### us-central-1
-
-resource "google_compute_router" "static_outbound_ip" {
-  name    = "static-outbound-ip-router"
-  network = google_compute_network.static_outbound_ip.id
-  region  = "us-central1"
-}
-
-resource "google_compute_address" "static_outbound_ip" {
-  name   = "static-outbound-ip"
-  region = "us-central1"
-}
-
-resource "google_compute_router_nat" "static_outbound_ip" {
-  name                               = "static-outbound-ip-nat"
-  region                             = "us-central1"
-  router                             = google_compute_router.static_outbound_ip.name
-  nat_ip_allocate_option             = "MANUAL_ONLY"
-  nat_ips                            = [google_compute_address.static_outbound_ip.self_link]
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_PRIMARY_IP_RANGES"
-}
-
 resource "google_compute_subnetwork" "static_outbound_ip_us" {
-  name          = "static-outbound-ip-subnet-us-central1"
+  name          = "static-outbound-ip-subnet-us"
   region        = "us-central1"
   network       = google_compute_network.static_outbound_ip.id
   ip_cidr_range = "10.0.0.0/28"
+}
+
+resource "google_compute_router" "static_outbound_ip_us" {
+  name    = "static-outbound-ip-router-us"
+  network = google_compute_network.static_outbound_ip.id
+  region  = "us-central1"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_compute_address" "static_outbound_ip_us" {
+  name   = "static-outbound-ip-address-us"
+  region = "us-central1"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_compute_router_nat" "static_outbound_ip_us" {
+  name                               = "static-outbound-ip-nat-us"
+  region                             = "us-central1"
+  router                             = google_compute_router.static_outbound_ip_us.name
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+  nat_ips                            = [google_compute_address.static_outbound_ip_us.self_link]
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_PRIMARY_IP_RANGES"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "google_vpc_access_connector" "static_outbound_ip_us" {
@@ -37,7 +48,8 @@ resource "google_vpc_access_connector" "static_outbound_ip_us" {
   # [名前] フィールドに、Compute Engine の命名規則に沿ってコネクタの名前を入力します。
   # ただし、名前の長さは 21 文字未満とし、ハイフン（-）は 2 文字としてカウントする必要があります。
   # Error creating Connector: googleapi: Error 400: Connector ID must follow the pattern ^[a-z][-a-z0-9]{0,23}[a-z0-9]$
-  name          = "static-ip-connector-us"
+  # 実際には25文字っぽい
+  name          = "static-ip-us"
   machine_type  = "f1-micro"
   min_instances = 2
   max_instances = 3
@@ -51,34 +63,46 @@ resource "google_vpc_access_connector" "static_outbound_ip_us" {
 ### asia-northeast1
 
 resource "google_compute_address" "static_outbound_ip_tokyo" {
-  name   = "static-outbound-ip"
+  name   = "static-outbound-ip-address-tokyo"
   region = "asia-northeast1"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "google_compute_router" "static_outbound_ip_tokyo" {
-  name    = "static-outbound-ip-router"
+  name    = "static-outbound-ip-router-tokyo"
   network = google_compute_network.static_outbound_ip.id
   region  = "asia-northeast1"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "google_compute_router_nat" "static_outbound_ip_tokyo" {
-  name                               = "static-outbound-ip-nat"
+  name                               = "static-outbound-ip-nat-tokyo"
   region                             = "asia-northeast1"
   router                             = google_compute_router.static_outbound_ip_tokyo.name
   nat_ip_allocate_option             = "MANUAL_ONLY"
   nat_ips                            = [google_compute_address.static_outbound_ip_tokyo.self_link]
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_PRIMARY_IP_RANGES"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "google_compute_subnetwork" "static_outbound_ip_tokyo" {
-  name          = "static-ip-connector-tokyo"
+  name          = "static-outbound-ip-subnet-tokyo"
   region        = "asia-northeast1"
   network       = google_compute_network.static_outbound_ip.id
   ip_cidr_range = "10.0.0.16/28"
 }
 
 resource "google_vpc_access_connector" "static_outbound_ip_tokyo" {
-  name          = "static-ip-connector-tokyo"
+  name          = "static-ip-tokyo"
   region        = "asia-northeast1"
   machine_type  = "e2-micro"
   min_instances = 2
@@ -92,14 +116,46 @@ resource "google_vpc_access_connector" "static_outbound_ip_tokyo" {
 ### asia-southeast1
 
 resource "google_compute_subnetwork" "static_outbound_ip_singapore" {
-  name          = "static-ip-connector-singapore"
+  name          = "static-ip-subnet-singapore"
   region        = "asia-southeast1"
   network       = google_compute_network.static_outbound_ip.id
   ip_cidr_range = "10.0.0.32/28"
 }
 
+resource "google_compute_address" "static_outbound_ip_singapore" {
+  name   = "static-outbound-ip-address-singapore"
+  region = "asia-southeast1"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_compute_router" "static_outbound_ip_singapore" {
+  name    = "static-outbound-ip-router-singapore"
+  network = google_compute_network.static_outbound_ip.id
+  region  = "asia-southeast1"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_compute_router_nat" "static_outbound_ip_singapore" {
+  name                               = "static-outbound-ip-nat-singapore"
+  region                             = "asia-southeast1"
+  router                             = google_compute_router.static_outbound_ip_singapore.name
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+  nat_ips                            = [google_compute_address.static_outbound_ip_singapore.self_link]
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_PRIMARY_IP_RANGES"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "google_vpc_access_connector" "static_outbound_ip_singapore" {
-  name          = "static-ip-connector-singa"
+  name          = "static-ip-singapore"
   region        = "asia-southeast1"
   machine_type  = "e2-micro"
   min_instances = 2
