10a5bd2d02482f0cef63e48862108fc5d12dd73b
Gosuke Miyashita <gosukenator@gmail.com>
2024-09-04 10:25:18 +0900

    Add terraform service account

--- projects/shared-vpc-host/iam.tf
+++ projects/shared-vpc-host/iam.tf
@@ -3,3 +3,34 @@ resource "google_organization_iam_member" "compute_xpnAdmin" {
   member = "user:miyashita@serverspec-operations.com"
   role   = "roles/compute.xpnAdmin"
 }
+
+data "google_service_account" "terraform" {
+  account_id = "terraform"
+}
+
+resource "google_project_iam_member" "terraform" {
+  for_each = toset([
+    "roles/resourcemanager.projectIamAdmin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/iam.roleViewer",
+    "roles/iam.roleAdmin",
+    "roles/storage.admin",
+    "roles/compute.networkAdmin",
+    "roles/vpcaccess.admin",
+  ])
+
+  project = data.google_project.current.project_id
+  role    = each.value
+  member  = "serviceAccount:${data.google_service_account.terraform.email}"
+}
+
+resource "google_organization_iam_member" "terraform" {
+  for_each = toset([
+    "roles/resourcemanager.organizationAdmin",
+    "roles/compute.xpnAdmin",
+  ])
+
+  org_id = data.google_organization.current.org_id
+  member = "serviceAccount:${data.google_service_account.terraform.email}"
+  role   = each.value
+}
